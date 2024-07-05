e1945060275c164c4c811cf17942f5779069fe30
Chennu05 <120401809+Chennu05@users.noreply.github.com>
2024-07-05 14:27:00 +0530

    Update book.py

--- src/book.py
+++ src/book.py
@@ -1,1 +1,11 @@
+# src/book.py
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.year})"
 
